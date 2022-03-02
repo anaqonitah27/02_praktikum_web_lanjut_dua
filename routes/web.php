e7c766b57a19e86223192d71a233d7a371954a94
anaqonitah27 <anaqonitah04@gmail.com>
2022-03-03 01:37:04 +0700

    Controller Kedua

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/about', [App\Http\Controllers\PageController::class, 'about']);
+Route::get('/about', [App\Http\Controllers\AboutController::class, 'about']);
 
-Route::get('/articles/{id?}', [App\Http\Controllers\PageController::class, 'articles']);
+Route::get('/articles/{id?}', [App\Http\Controllers\ArticleController::class, 'articles']);
 
